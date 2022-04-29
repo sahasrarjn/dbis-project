4bc88a873b0ce2c62513084d24536f4d5dc72318
Sahasra Ranjan <sahasraranjan@gmail.com>
2022-04-29 13:29:45 +0530

    .

--- project/frontend/src/app/question-data/question-data.component.ts
+++ project/frontend/src/app/question-data/question-data.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MainService } from '../services/main.service';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 
 @Component({
@@ -14,34 +15,63 @@ import { BaseChartDirective } from 'ng2-charts';
 export class QuestionDataComponent implements OnInit {
   id : any;
   question : any;
-  current_insti;
-
-  public timeTakenChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-  };
-  public timeTakenChartLabels: string[] = [ 'Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running' ];
-
-  public timeTakenChartData: ChartData<'scatter'> = {
-    labels: this.timeTakenChartLabels,
-    datasets: [
-      {
-        data: [
-          { x: 1, y: 1 },
-          { x: 2, y: 3 },
-          { x: 3, y: -2 },
-          { x: 4, y: 4 },
-          { x: 5, y: -3 },
-        ],
-        label: 'Series A',
-        pointRadius: 10,
-      },
-    ]
-  };
-  public timeTakenChartType: ChartType = 'scatter';
-
+  current_insti : any;
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {}
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [
+    DataLabelsPlugin
+  ];
+
+  public time_taken_data: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Number of students vs Time taken' }
+    ]
+  };
+
+  public marks_data: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Number of students vs Marks obtained' }
+    ]
+  };
+
+  public relevance_data: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Number of students vs Relevance' }
+    ]
+  };
+
+  
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    // console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    // console.log(event, active);
+  }
   constructor(private route : ActivatedRoute, private ms : MainService, private router: Router) {
   }
 
@@ -49,7 +79,9 @@ export class QuestionDataComponent implements OnInit {
     this.route.params.subscribe(params => { this.id = params['id'] });
     this.ms.getQuestionData(this.id).subscribe(data => {
       this.question = data;
-      console.log(this.question);
+      this.setTimeTakenData(data['student_data']);
+      this.setMarksData(data['student_data']);
+      this.setRelevanceData(data['student_data']);
     });
   }
 
@@ -57,6 +89,65 @@ export class QuestionDataComponent implements OnInit {
     this.router.navigate([url]);
   }
 
+  setTimeTakenData(data){
+    var map = new Map();
+
+    for(let i = 0; i < data.length; i++){
+      var d = data[i];
+      var key : number = parseInt(d['time_taken']);
+      if(map.has(key)){
+        map.set(key, map.get(key) + 1);
+      }else{
+        map.set(key, 1);
+      }
+    }
+
+    var arr = Array.from(map.entries()).sort((a, b) => a[0] - b[0]);
+    for (let entry of arr){
+      this.time_taken_data.labels.push(entry[0]);
+      this.time_taken_data.datasets[0].data.push(entry[1]);
+    }
+  }
+
+  setMarksData(data){
+    var map = new Map();
+
+    for(let i = 0; i < data.length; i++){
+      var d = data[i];
+      var key : number = parseFloat(d['marks']);
+      if(map.has(key)){
+        map.set(key, map.get(key) + 1);
+      }else{
+        map.set(key, 1);
+      }
+    }
+
+    var arr = Array.from(map.entries()).sort((a, b) => a[0] - b[0]);
+    for (let entry of arr){
+      this.marks_data.labels.push(entry[0]);
+      this.marks_data.datasets[0].data.push(entry[1]);
+    }
+  }
+
+  setRelevanceData(data){
+    var map = new Map();
+
+    for(let i = 0; i < data.length; i++){
+      var d = data[i];
+      var key : number = parseInt(d['relevance']);
+      if(map.has(key)){
+        map.set(key, map.get(key) + 1);
+      }else{
+        map.set(key, 1);
+      }
+    }
+
+    var arr = Array.from(map.entries()).sort((a, b) => a[0] - b[0]);
+    for (let entry of arr){
+      this.relevance_data.labels.push(entry[0]);
+      this.relevance_data.datasets[0].data.push(entry[1]);
+    }
+  }
 
 
 
